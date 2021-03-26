228f9fdce627eac7045b30db0b07ad5fae484a69
Henry <henrygraves11@yahoo.com>
2021-03-26 23:39:52 +0000

    Add prototype table and chart.

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -26,3 +26,51 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+// Google Charts Library @ https://developers.google.com/chart 
+// ---------------------------------------------------------------
+google.charts.load('current', {'packages':['corechart', 'line']});
+google.charts.setOnLoadCallback(drawChart);
+
+// Create a line chart and add it to the page.
+function drawChart() {
+    const data = new google.visualization.DataTable();
+    data.addColumn('string', 'Year');
+    data.addColumn('number', '%');
+        data.addRows([
+            ['1999', 58],
+            ['2000', 62],
+	        ['2001', 62],
+	        ['2002', 60],
+	        ['2003', 60],
+	        ['2004', 61],
+            ['2005', 62],
+            ['2006', 61],
+            ['2007', 65],
+            ['2008', 62],
+            ['2009', 57],
+            ['2010', 56],
+            ['2011', 54],
+            ['2012', 53],
+            ['2013', 52],
+            ['2014', 54],
+            ['2015', 55],
+            ['2016', 52],
+            ['2017', 54],
+            ['2018', 55],
+            ['2019', 55],
+            ['2020', 55],
+        ]);
+
+    const options = {
+        'title': 'Percentage of American adults investing in the stock market from 1999 - 2020',
+        'legend': {position:'none'},
+        'backgroundColor': 'WhiteSmoke', 
+        'colors':['#84a98c'],  
+        'width':801,
+        'height':400
+    };
+    
+    const chart = new google.visualization.LineChart(document.getElementById('chart-container'));
+    chart.draw(data, options);
+}
